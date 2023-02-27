899449fb1a0e78280d0fc7ab21172c9245bfae4b
Мария Гречковская <mariya.grechkovskaya@spellabs.ru>
2023-02-27 15:27:42 +0300

    Add project files.

---
+++ mantis-tests/AppManager/RegistrationHelper.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace mantis_tests
+{
+    public class RegistrationHelper : HelperBase
+    {
+        public RegistrationHelper(ApplicationManager manager) : base(manager) { }
+
+        public void Register(AccountData account)
+        {
+            OpenMainPage();
+            OpenRegistrationForm();
+            FillRegistrationForm(account);
+            SubmitRegistration();
+
+        }
+
+        private void OpenRegistrationForm()
+        {
+            driver.FindElement(By.LinkText("Signup for a new account")).Click();
+        }
+        private void SubmitRegistration()
+        {
+            driver.FindElement(By.XPath("//input[@value='Signup']")).Click();
+        }
+        private void FillRegistrationForm(AccountData account)
+        {
+            driver.FindElement(By.Id("username")).SendKeys(account.Name);
+            driver.FindElement(By.Id("email-field")).SendKeys(account.Email);
+        }
+        private void OpenMainPage()
+        {
+            manager.Driver.Url = "http://localhost/mantisbt-2.25.6/login_page.php";
+        }
+    }
+}
---
+++ mantis-tests/Tests/AccountCreationTests.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace mantis_tests 
+{
+    [TestFixture]
+    public class AccountCreationTests : TestBase
+    {
+        [TestFixtureSetUp]
+        public void setUpConfig()
+        {
+            app.Ftp.BackupFile("/config_inc.php");
+            using (Stream localFile = File.Open("config_inc.php", FileMode.Open))
+            {
+                app.Ftp.Upload("/config_defaults_inc.php", localFile);
+            }
+        }
+
+        [Test]
+        public void TestAccountRegistration()
+        {
+            AccountData account = new AccountData()
+            {
+                Name = "testuser",
+                Password = "password",
+                Email = "testuser@localhost.localdomain"
+
+
+            };
+
+            app.Registration.Register(account);
+
+        }
+
+        [TestFixtureTearDown]
+        public void restoreConfig()
+        {
+            app.Ftp.RestoreBackupFile("/config_inc.php");
+        }
+    }
+}
